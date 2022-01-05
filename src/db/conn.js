b81845af32a0213dd0e3748444cd63f23cdce5e6
unknown <srroy119@gmail.com>
2022-01-05 17:22:53 +0530

    update db

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/blogweb').then(()=>{
+mongoose.connect('mongodb://localhost:27017/blogweb',{
+    stale: false,
+    compatible: true,
+    heartbeatFrequencyMS: 10000,
+    localThresholdMS: 15,
+    logicalSessionTimeoutMinutes: undefined,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
     console.log(`connection successful..`);
 }).catch((err)=>{
     console.log(err);
